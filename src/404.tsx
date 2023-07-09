3405d6c731c3460a0b6090c300da5405442c0952
dbfu <fudebao@byteplan.com>
2023-07-09 13:16:06 +0800

    feat:动态菜单动态路由实现

--- src/404.tsx
+++ src/404.tsx
@@ -5,10 +5,10 @@ const Result404 = () => (
   <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
+    subTitle="对不起，你访问的页面不存在。"
     extra={(
       <Button type="primary">
-        <Link to="/">Back Home</Link>
+        <Link to="/">首页</Link>
       </Button>
     )}
   />
