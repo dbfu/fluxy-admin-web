bdc056c546d44d1ac06c6ba74f67091e9e20d71c
dbfu <fudebao@byteplan.com>
2023-07-11 00:09:04 +0800

    feat:动态菜单动态路由实现

--- src/components/with-auth/index.tsx
+++ src/components/with-auth/index.tsx
@@ -1,13 +1,9 @@
 import { isAuth } from '@/utils/auth';
-import { PureComponent, ReactNode } from 'react';
 
 export function withAuth(authCode: string) {
-  return class extends PureComponent<{ children: React.ReactElement }> {
-    render(): ReactNode {
-      if (isAuth(authCode)) {
-        const { children } = this.props;
-        return children;
-      }
+  return function (Component: any) {
+    return function (props: any) {
+      return isAuth(authCode) ? <Component {...props}></Component> : null;
     }
   }
 }
