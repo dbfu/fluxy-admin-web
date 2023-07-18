8287f156389f8c752ca44cbd2965d1123a94b043
dbfu <fudebao@byteplan.com>
2023-07-18 22:11:15 +0800

    feat:集成websocket实现权限变更消息推送，页面自动刷新。

--- nginx/config.sh
+++ nginx/config.sh
@@ -40,7 +40,7 @@ cat >> /etc/nginx/conf.d/default.conf <<EOF
     }
 
     location /api {
-        proxy_pass  $SERVER_URL;
+        proxy_pass $SERVER_URL;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
@@ -54,10 +54,10 @@ cat >> /etc/nginx/conf.d/default.conf <<EOF
     }
 
     location /ws/ {
-      proxy_pass $SOCKET_URL;
+      proxy_pass $SERVER_URL;
       proxy_http_version 1.1;
       proxy_set_header Upgrade \$http_upgrade;
-      proxy_set_header Connection "Upgrade";
+      proxy_set_header Connection \$connection_upgrade;
       proxy_set_header Host \$host;
       proxy_set_header X-Real-IP \$remote_addr;
       proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
